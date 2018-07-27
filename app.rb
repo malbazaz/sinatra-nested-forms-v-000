745cb5fdf756c9e92c6cb8f1c93a8ccfecc3e070
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 17:56:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,15 +12,11 @@ module FormsLab
     end
 
     post "/pirates" do
-      @pirate_name  = Pirate.new(params[:name])
-      @pirate_weight= Pirate.new(params[:weight])
-      @pirate_height= Pirate.new(params[:height])
-      @ship_name_1  = Ship.new(params[:name])
-      @ship_type_1  = Ship.new(params[:type])
-      @ship_type_1  = Ship.new(params[:boot])
-      @ship_name_2  = Ship.new(params[:name])
-      @ship_type_2  = Ship.new(params[:type])
-      @ship_booty_2 = Ship.new(params[:booty])
+      @pirate = Pirate.new(params[:student])
+          params[:student][:courses].each do |details|
+            Course.new(details)
+          end
+        @courses = Course.all
       erb :'pirates/show'
     end
 
