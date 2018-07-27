8934ed4217e509d698501f525ce89762aa796491
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 17:56:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ module FormsLab
 
     post "/pirates" do
       @pirate = Pirate.new(params[:pirate])
-          params[:pirate][:ship].each do |details|
+          params[:pirate][:ships].each do |details|
             Course.new(details)
           end
         @courses = Course.all
