cd64e1d565a1e93e7533f91fa64d2440fe5a3055
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 17:27:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get "/new" do 
+      erb :new 
+    end 
+
+    post "/post" do 
+      erb :show
+    end 
+  
     # code other routes/actions here
 
   end
